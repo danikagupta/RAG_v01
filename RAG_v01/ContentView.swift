d531a661955cae290a00c84cab4b07eb12f4ebd1
Danika Gupta <danika@gprof.com>
2023-11-04 22:11:56 -0700

    Initial Commit

---
+++ RAG_v01/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RAG_v01
+//
+//  Created by Amit Gupta on 11/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RAG_v01/RAG_v01App.swift
@@ -0,0 +1,17 @@
+//
+//  RAG_v01App.swift
+//  RAG_v01
+//
+//  Created by Amit Gupta on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct RAG_v01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
